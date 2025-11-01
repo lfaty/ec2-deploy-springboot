3f6a845811d22378fb3398a6143ffd1e365dbe68
lfaty <faty.laminefaty@gmail.com>
2025-11-01 12:51:43 +0000

    deleted ec2-deploy.yml

---
+++ src/main/java/org/uasz/ec2_deploy_springboot/WelcomeController.java
@@ -0,0 +1,15 @@
+package org.uasz.ec2_deploy_springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @RequestMapping("/")
+    public String welcome(){
+        return "Hello Everyone";
+    }
+
+}
